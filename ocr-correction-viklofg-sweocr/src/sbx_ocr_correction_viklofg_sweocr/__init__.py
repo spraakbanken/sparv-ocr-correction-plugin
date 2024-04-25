8df9428f446d6b1654020bbd3461ffd4c0677f95
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-04-25 09:51:18 +0200

    fix: indexerror single word

--- ocr-correction-viklofg-sweocr/src/sbx_ocr_correction_viklofg_sweocr/__init__.py
+++ ocr-correction-viklofg-sweocr/src/sbx_ocr_correction_viklofg_sweocr/__init__.py
@@ -49,7 +49,8 @@ def annotate_ocr_correction(
         sent_to_tag = [token_word[token_index] for token_index in sent]
 
         ocr_corrections = ocr_corrector.calculate_corrections(sent_to_tag)
-        out_ocr_correction_annotation[:] = ocr_corrections
+        for i, ocr_correction in enumerate(ocr_corrections):
+            out_ocr_correction_annotation[i] = ocr_correction
 
     logger.info("writing annotations")
     out_ocr_correction.write(out_ocr_correction_annotation)
@@ -77,33 +78,38 @@ class OcrCorrector:
 
     def calculate_corrections(self, text: List[str]) -> List[Optional[str]]:
         logger.debug("Analyzing '%s'", text)
-        parts = []
-        curr_part: List[str] = []
-        curr_len = 0
         ocr_corrections: List[str] = []
-        for word in text:
-            len_word = bytes_length(word)
-            if (curr_len + len_word + 1) > self.TEXT_LIMIT:
-                parts.append(TOK_SEP.join(curr_part))
-                curr_part, curr_len = [word], len_word
-            else:
-                curr_part.append(word)
-                curr_len = len_word if curr_len == 0 else curr_len + len_word + 1
-        if len(curr_part) > 0:
-            parts.append(TOK_SEP.join(curr_part))
-        for part in parts:
-            suggested_text = self.pipeline(part)[0]["generated_text"]
-            suggested_text = suggested_text.replace(",", " ,")
-            suggested_text = suggested_text.replace(".", " .")
-            ocr_corrections = ocr_corrections + suggested_text.split(TOK_SEP)
+        for s in text:
+            logger.debug("word = %s", s)
+            sugg = self.pipeline(s)[0]["generated_text"]
+            ocr_corrections.append(sugg)
+        # parts = []
+        # curr_part: List[str] = []
+        # curr_len = 0
+        # ocr_corrections: List[str] = []
+        # for word in text:
+        #     len_word = bytes_length(word)
+        #     if (curr_len + len_word + 1) > self.TEXT_LIMIT:
+        #         parts.append(TOK_SEP.join(curr_part))
+        #         curr_part, curr_len = [word], len_word
+        #     else:
+        #         curr_part.append(word)
+        #         curr_len = len_word if curr_len == 0 else curr_len + len_word + 1
+        # if len(curr_part) > 0:
+        #     parts.append(TOK_SEP.join(curr_part))
+        # for part in parts:
+        #     suggested_text = self.pipeline(part)[0]["generated_text"]
+        #     suggested_text = suggested_text.replace(",", " ,")
+        #     suggested_text = suggested_text.replace(".", " .")
+        #     ocr_corrections = ocr_corrections + suggested_text.split(TOK_SEP)
 
-        if len(text) == len(ocr_corrections) + 1 and text[-1] != ocr_corrections[-1]:
-            ocr_corrections.append(text[-1])
+        # if len(text) == len(ocr_corrections) + 1 and text[-1] != ocr_corrections[-1]:
+        #     ocr_corrections.append(text[-1])
         return zip_and_diff(text, ocr_corrections)
 
 
 def zip_and_diff(orig: List[str], sugg: List[str]) -> List[Optional[str]]:
-    return [sw if sw != ow else None for (ow, sw) in zip(orig, sugg)]
+    return [sw if sw != ow else None for (ow, sw) in zip(orig, sugg, strict=True)]
 
 
 def bytes_length(s: str) -> int:
